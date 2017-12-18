15bd914baa1c673ab13802f1a07e342c6bb71de7
Jesse <jcassill@bofifederalbank.com>
2017-12-18 07:51:48 -0800

    new data

--- parse.py
+++ parse.py
@@ -4,11 +4,19 @@ import requests
 import time
 
 
-def get_file(which_file):
+# def get_file(which_file):
+#     data_dir = '.\\data\\'    
+#     url = 'http://www.calottery.com/sitecore/content/Miscellaneous/download-numbers/?GameName=' + which_file +'&Order=Yes'
+#     resp = requests.get(url)
+#     with open(data_dir + which_file + '.txt', 'wb') as pbf:
+#         pbf.write(resp.content)
+
+
+def get_file(which_game, ordered='No', fname='newdata'):
     data_dir = '.\\data\\'    
-    url = 'http://www.calottery.com/sitecore/content/Miscellaneous/download-numbers/?GameName=' + which_file +'&Order=Yes'
+    url = 'http://www.calottery.com/sitecore/content/Miscellaneous/download-numbers/?GameName=' + which_game +'&Order=' + ordered
     resp = requests.get(url)
-    with open(data_dir + which_file + '.txt', 'wb') as pbf:
+    with open(data_dir + fname + '.txt', 'wb') as pbf:
         pbf.write(resp.content)
 
 
@@ -37,7 +45,7 @@ def parse_line(line):
 def mkcsv(fname):
     data_dir = '.\\data\\'
     hdr_size = -5
-    get_file(fname)
+#    get_file(game, 'Yes', fname)
     lines = list(reversed(parse_file(fname)))
     with open(data_dir + fname + '.csv', 'wt') as csvfile:
         for line in lines[:hdr_size]:
@@ -45,6 +53,10 @@ def mkcsv(fname):
             csvfile.write("\n")
 
 
-fnames = ['powerball', 'mega-millions', 'superlotto-plus']
-for name in fnames:
-    mkcsv(name)
+# fnames = ['powerball', 'mega-millions', 'superlotto-plus']
+# for name in fnames:
+#     mkcsv(name)
+fn = {'powerball':'pball', 'mega-millions':'mega', 'superlotto-plus':'sball'}
+for game,fname in fn.items():
+    get_file(game,'Yes',fname)
+    mkcsv(fname)
